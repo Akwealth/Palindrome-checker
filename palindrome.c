abce45db33ac51661d92a1b972232d5ecd2f8283
Gabriel Isaac <stewarti3@yahoo.com>
2026-03-11 00:03:48 +0100

    refactor: improve comments and formatting

--- palindrome.c
+++ palindrome.c
@@ -2,36 +2,30 @@
 A C program to find whether given number is a palindrome or not.
 
 -------------------------------------------------------------
-PSEUDO CODE
+PROGRAM PSEUDO CODE
 
 BEGIN
-
-    // Step 1: Declare variables
     DECLARE num, originalNum, reversedNum, remainder AS INTEGER
 
-    // Step 2: Take input from the user
     PRINT "Enter an integer: "
     READ num
 
-    // Step 3: Store the original number for later comparison
     SET originalNum = num
     SET reversedNum = 0
 
-    // Step 4: Reverse the number using a loop
     WHILE num != 0 DO
-        remainder = num MOD 10          // Extract the last digit
-        reversedNum = reversedNum * 10 + remainder  // Append digit to reversed number
-        num = num / 10                  // Remove the last digit from num
+        remainder = num MOD 10
+        reversedNum = reversedNum * 10 + remainder  
+        num = num / 10               
     END WHILE
 
-    // Step 5: Compare the reversed number with the original
     IF originalNum == reversedNum THEN
         PRINT originalNum, " is a Palindrome."
     ELSE
         PRINT originalNum, " is NOT a Palindrome."
     END IF
-
 END
+
 */
 
 #include <stdio.h>
@@ -48,7 +42,7 @@ int main(){
     // Reverse the number using a while loop
     while (num != 0){
         remainder = num % 10; // this extracts last digit of the value
-        reversedNum = reversedNum * 10 + remainder;
+        reversedNum = reversedNum * 10 + remainder; // Append digit to reversed number
         num = num / 10; // remove last digit from the num
     }
 
